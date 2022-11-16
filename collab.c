ea597781942c885057d7bd6fd0dacb177a2db464
Yamaan Nandolia <88333832+YamaanNandolia@users.noreply.github.com>
2022-11-16 08:41:28 -0600

    Command line
    Labwork successfully committed using the command line

---
+++ collab.c
@@ -0,0 +1,39 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void bleyva3_ynand3() {
+    printf("Brenda Leyva and yamaan Nandolia\n");
+    prntf("We learned that GitHub is a cloud-based hosting service that lets you manage Git repositories.\n")
+    printf("If you have open-source projects that use Git, then GitHub is designed to help you better manage them.\n")
+}
+
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    bleyva3_ynand3();
+    
+    return 0;
+}
